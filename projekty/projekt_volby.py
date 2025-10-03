3d7e4230ae315143c2d051b570b0435d8f41786e
berox455 <kaja.k.brno@gmail.com>
2025-10-03 10:32:33 +0200

    rename random.py into ulohy_random.py, create projekt_volby.py with starting stuff

---
+++ projekty/projekt_volby.py
@@ -0,0 +1,36 @@
+import random
+
+
+class Vote_simulator:
+    def __init__(self):
+        self.preference = pole_s_preferenci #docasne
+        self.people_count = cislo #docasne
+
+
+    def add_noise(self):
+        return noise
+
+    
+    def gen_participation(self):
+        return participation 
+
+    
+    def gen_voters(self):
+        return voters 
+
+
+    def print_party_stats(self):
+        return stats
+
+
+    def gen_voter_chart(self):
+        return chart 
+
+    
+    def mandate_counter(self):
+        return count
+
+
+    def play(self):
+        return simulation
+    
